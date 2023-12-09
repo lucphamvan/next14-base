6662a34f3671e58ffa5e6fc6774086e2262affff
Pham Luc <tchh.lucpham@gmail.com>
2023-12-10 00:04:29 +0700

    fix: prevent flick when enter page with dark mode

--- components/provider/index.tsx
+++ components/provider/index.tsx
@@ -3,6 +3,7 @@
 import { NextAuthProvider } from "@/components/nextauth-provider"
 import { CacheProvider } from "@chakra-ui/next-js"
 import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
 
 import ThemeProvider from "./ThemeProvider"
 import { customTheme } from "./theme-config"
@@ -10,6 +11,16 @@ import { customTheme } from "./theme-config"
 export const theme = extendTheme(customTheme)
 
 export function Provider({ children }: { children: React.ReactNode }) {
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
+
     return (
         <NextAuthProvider>
             <CacheProvider>
