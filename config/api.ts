2a9a2d34b672fb0d307995bd7da49df41264b139
jugmirana <jug.mirana@gmail.com>
2024-10-17 22:56:56 +0700

    update for production

--- config/api.ts
+++ config/api.ts
@@ -1,17 +1,17 @@
 import axios from "axios"
 
-const baseURL_CALL_FROM_FE = "http://localhost:7070"
-const URL_CALL_FROM_BE_NEXTJS = "hhttp://192.168.20.144:7070"
+const BASEURL = "https://hintapi.ltool.pro"
+const BE_URL = "https://hintapi.ltool.pro"
 
 export const api = axios.create({
-    baseURL: baseURL_CALL_FROM_FE
+    baseURL: BASEURL
 })
 
 export enum API {
-    LOGIN = URL_CALL_FROM_BE_NEXTJS + "/users/login", // this api call from BE of nextjs => call in docker => if use localhost, it will not work
-    REFRESH_TOKEN = URL_CALL_FROM_BE_NEXTJS + "/token/refresh", // this api call from BE of nextjs => call in docker => if use localhost, it will not work
+    LOGIN = BE_URL + "/users/login", // this api call from BE of nextjs => call in docker => if use localhost, it will not work
+    REFRESH_TOKEN = BE_URL + "/token/refresh", // this api call from BE of nextjs => call in docker => if use localhost, it will not work
     GET_USER = "/users/me",
     GET_USERS = "/users",
     CREATE_USER = "/users",
-    HINTDATA = baseURL_CALL_FROM_FE + "/accounts"
+    HINTDATA = BASEURL + "/accounts"
 }
