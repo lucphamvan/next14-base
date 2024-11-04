aa7f173c91efa96c24b305488d9d338e17ec6f0f
Pham Luc <jug.mirana@gmail.com>
2024-11-04 14:56:34 +0700

    update

--- components/layout/mobile-menu/index.styled.tsx
+++ components/layout/mobile-menu/index.styled.tsx
@@ -9,7 +9,7 @@ export const Overlay = styled.div<{ $isShow: boolean }>`
     left: 0;
     top: 0;
     overflow-x: hidden;
-    transition: width 0.3s cubic-bezier(0.075, 0.82, 0.165, 1);
+    transition: width 0.25s cubic-bezier(0.075, 0.82, 0.165, 1);
 `
 
 export const Container = styled.div<{ $isShow: boolean }>`
@@ -18,7 +18,7 @@ export const Container = styled.div<{ $isShow: boolean }>`
     max-width: 300px;
     background-color: ${({ theme }) => theme.Color.BgMain};
     padding: 0;
-    transition: all 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
+    transition: all 0.25s cubic-bezier(0.075, 0.82, 0.165, 1);
 `
 
 export const Wrapper = styled.div`
